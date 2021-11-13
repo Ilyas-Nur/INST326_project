b4cec839e780dae492b772eadd66b88043e59ea8
Ilyas Nur <inur@terpmail.umd.edu>
2021-11-13 15:52:40 -0500

    My method implemeting a new idea using current dates

--- Check_in_1.py
+++ Check_in_1.py
@@ -5,7 +5,7 @@ def holiday_szn():
 
         This method determines whether the current day we're in is close to a holiday
 
-        If the day is past or on the 20th of the month of November, its Thanksgiving season
+        If the day is past or on the 15th of the month of November, its Thanksgiving season
         
         If the day is past or on the 15th of the month of December, its Christmas season
         
@@ -13,6 +13,9 @@ def holiday_szn():
         
         If not, returns None (Falsy)
         
+        Args:
+            x (optionl):A date if you would actually like to specify. Could be handy if you
+            are planning to buy a car that dar
         
         Returns:
             True or False: Depending on the currrent day and month
@@ -23,23 +26,28 @@ def holiday_szn():
     x = datetime.datetime.now()
     
     if x.strftime("%B") == "November":
-        if int(x.strftime("%d")) >= 20:
+        if int(x.strftime("%d")) >= 15:
             print("It's almost Turkey day! Here's a discount")
             return True
+        else:
+            print("Its not the holidays, so unfortuntely no discount")
         
     elif x.strftime("%B") == "December":
         if int(x.strftime("%d")) >= 15:
             print("It's almost Christmas. Your present is a discount!")
             return True
+        else:
+            print("Its not the holidays just yet!, so unfortuntely no discount")
         
     elif x.strftime("%B") == "January":
         if int(x.strftime("%d")) <= 15:
             print("We are still in the holiday spirit from the New Year, have a discount!")
             return True
+        else:
+            print("Its not the holidays anymore, so unfortuntely no discount")
         
-    else:
+    elif x.strftime("%B") != "November" and x.strftime("%B") != "December" and x.strftime("%B") != "January":
         print("Its not the holidays, so unfortuntely no discount")
         return False
     
-
-       
+print(holiday_szn())
